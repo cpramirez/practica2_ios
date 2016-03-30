e651df19a12869e723d29e9190e56256e52403e9
CICE <cice@aula03mac01.local>
2016-03-30 16:17:49 +0200

    Initial Commit

---
+++ AppTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppTest2
+//
+//  Created by CICE on 30/3/16.
+//  Copyright © 2016 CICE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
